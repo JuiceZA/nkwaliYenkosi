de504dfb3c1be8c6f1bdfc24e4c9f48a1be27b37
Blaine <blainefoster12@gmail.com>
2022-11-13 23:01:37 +0200

    Shop page and checkout page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 function show(){
     document.getElementById('sidebar').classList.toggle('active');
 }
+
+
+var item = document.getElementsByClassName("itemName").innerHTML
+function shopButton(){
+    var buttonId = document.getElementsByTagName("button").id
+    var price = document.getElementById("price1,1").innerHTML;
+    var amount = prompt("How many would you like?")
+    if (amount === true){
+        var result = confirm('Proceed to checkout');
+    }
+    if (result){
+        console.log("Its the truth")
+    }
+    console.log(price)
+    console.log(buttonId)
+};
